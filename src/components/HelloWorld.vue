8e52fad234879a9c016c2d1967e5bc0d325667a9
madalinaeleonorag <madalinaeleonora.gheorghe@gmail.com>
2020-07-18 15:14:09 +0300

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,61 +2,138 @@
   <v-container>
     <v-layout wrap>
       <!-- ADD TODO -->
-      <v-flex xs12 sm6 md6>
-        <v-text-field label="What you want ToDo?" solo :items="toDos" v-model="newToDo"></v-text-field>
-      </v-flex>
-      <v-flex xs12 sm6 md6>
-        <v-select
-          solo
-          :items="priorities"
-          v-model="selectedPriority"
-          item-value="selectedPriority"
-          label="Select priority"
-        ></v-select>
-      </v-flex>cd ..
       <v-flex xs12 sm12 md12>
-        <v-btn block @click="AddToDo">ADD</v-btn>
+        <div
+          style="font-size: 50px; color: white; margin-top: 5%; margin-bottom: 3%; text-align: center"
+        >
+          ToDoList
+        </div>
+      </v-flex>
+      <v-flex xs12 sm12 md12 row justify-center>
+        <v-flex xs12 sm6 md4>
+          <v-text-field
+            label="What you want ToDo?"
+            solo
+            :items="toDos"
+            v-model="newToDo"
+            style="margin-right: 10px"
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm1 md>
+          <v-select
+            solo
+            :items="priorities"
+            v-model="selectedPriority"
+            item-value="selectedPriority"
+            label="Select priority"
+          ></v-select>
+        </v-flex>
+        <v-flex xs12 sm1 md1>
+          <v-btn
+            class="mx-2"
+            fab
+            dark
+            color="indigo"
+            @click="AddToDo"
+            small
+            style="float: right"
+          >
+            <v-icon dark>mdi-plus</v-icon>
+          </v-btn>
+        </v-flex>
       </v-flex>
       <!-- SELECT PRIORITY -->
-      <v-flex xs3 sm3 md3>
-        <v-checkbox label="All" v-model="all" @change="checkAll"></v-checkbox>
-      </v-flex>
-      <v-flex xs3 sm3 md3>
-        <v-checkbox label="High" v-model="high" @change="check"></v-checkbox>
-      </v-flex>
-      <v-flex xs3 sm3 md3>
-        <v-checkbox label="Medium" v-model="medium" @change="check"></v-checkbox>
-      </v-flex>
-      <v-flex xs3 sm3 md3>
-        <v-checkbox label="Low" v-model="low" @change="check"></v-checkbox>
+      <v-flex xs12 sm12 md12 row justify-center>
+        <v-flex xs12 sm12 md6 row justify-center>
+          <v-flex xs3 sm3 md3>
+            <v-checkbox
+              label="All"
+              v-model="all"
+              @change="checkAll"
+            ></v-checkbox>
+          </v-flex>
+          <v-flex xs3 sm3 md3>
+            <v-checkbox
+              label="High"
+              v-model="high"
+              @change="check"
+            ></v-checkbox>
+          </v-flex>
+          <v-flex xs3 sm3 md3>
+            <v-checkbox
+              label="Medium"
+              v-model="medium"
+              @change="check"
+            ></v-checkbox>
+          </v-flex>
+          <v-flex xs3 sm3 md3>
+            <v-checkbox label="Low" v-model="low" @change="check"></v-checkbox>
+          </v-flex>
+        </v-flex>
       </v-flex>
       <!-- LIST TODO -->
-      <v-flex xs12 sm12 md12>
-        <v-list v-for="(item, index) in items" :key="index">
-          <v-card>
+      <v-flex xs12 sm12 md12 row justify-center>
+        <v-flex xs12 sm12 md6>
+          <v-card
+            v-for="(item, index) in items"
+            :key="index"
+            style="margin-bottom: 10px; opacity: 0.7"
+          >
             <v-card-text>
-              <div v-if="item.Status === 'Done'" class="green--text">{{item.Text}}</div>
-              <div v-else>{{item.Text}}</div>
+              <v-flex row>
+                <v-flex xs12 sm9 md9>
+                  <div v-if="item.Status === 'Done'" class="green--text">
+                    {{ item.Text }}
+                  </div>
+                  <div v-else>{{ item.Text }}</div>
+                </v-flex>
+                <v-flex xs12 sm3 md3>
+                  <div
+                    v-if="item.Priority === 'High'"
+                    class="red--text"
+                    style="float:right"
+                  >
+                    {{ item.Priority }}
+                  </div>
+                  <div
+                    v-if="item.Priority === 'Medium'"
+                    class="orange--text"
+                    style="float:right"
+                  >
+                    {{ item.Priority }}
+                  </div>
+                  <div
+                    v-if="item.Priority === 'Low'"
+                    class="yellow--text"
+                    style="float:right"
+                  >
+                    {{ item.Priority }}
+                  </div>
+                </v-flex>
+              </v-flex>
             </v-card-text>
             <v-card-actions>
-              <v-card-text>
-                <div v-if="item.Priority === 'High'" class="red--text">{{item.Priority}}</div>
-                <div v-if="item.Priority === 'Medium'" class="orange--text">{{item.Priority}}</div>
-                <div v-if="item.Priority === 'Low'" class="yellow--text">{{item.Priority}}</div>
-              </v-card-text>
+              <v-card-text> </v-card-text>
               <v-spacer></v-spacer>
-              <v-btn :disabled="item.Status === 'Done'">
-                <v-icon @click="doneToDo(index)" color="green">done</v-icon>
+
+              <v-btn :disabled="item.Status === 'Done'" fab small>
+                <v-icon @click="doneToDo(index)" color="green"
+                  >mdi-check</v-icon
+                >
               </v-btn>
-              <v-btn :disabled="item.Status === 'Done'">
-                <v-icon @click="editToDo(index)" color="orange">create</v-icon>
+              <v-btn :disabled="item.Status === 'Done'" fab small>
+                <v-icon @click="editToDo(index)" color="orange"
+                  >mdi-pencil</v-icon
+                >
               </v-btn>
-              <v-btn>
-                <v-icon @click="deleteToDo(index)" color="red">delete</v-icon>
+              <v-btn fab small>
+                <v-icon @click="deleteToDo(index)" color="red"
+                  >mdi-delete</v-icon
+                >
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-list>
+        </v-flex>
       </v-flex>
     </v-layout>
 
@@ -92,13 +169,13 @@ export default {
       all: true,
       high: false,
       medium: false,
-      low: false
+      low: false,
     };
   },
   computed: {
     items() {
       var filteredItems = [];
-      filteredItems = this.toDos.filter(todo => {
+      filteredItems = this.toDos.filter((todo) => {
         if (this.all === true) {
           return true;
         } else if (this.high === true && todo.Priority === "High") {
@@ -111,14 +188,14 @@ export default {
         }
       });
       return filteredItems;
-    }
+    },
   },
   methods: {
     AddToDo() {
       this.toDos.push({
         Text: this.newToDo,
         Status: "incomplete",
-        Priority: this.selectedPriority
+        Priority: this.selectedPriority,
       });
       this.newToDo = "";
     },
@@ -148,7 +225,7 @@ export default {
       if (this.high === true || this.medium === true || this.low === true) {
         this.all = false;
       }
-    }
+    },
   },
   mounted() {
     console.log("tododo");
@@ -161,8 +238,8 @@ export default {
         console.log("Todos changed!");
         localStorage.setItem("toDos", JSON.stringify(this.toDos));
       },
-      deep: true
-    }
-  }
+      deep: true,
+    },
+  },
 };
 </script>
